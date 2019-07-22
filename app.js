0efe1a527811eb0a9f73c26a227cb15a8d2a1f2d
Kevin <anyboo@qq.com>
2019-07-22 18:23:03 +0800

    增加RESTful接口模版
    详细参考https://github.com/anyboo/UniformAdminDashboard/wiki/Restful-API-Document

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var scanRouter = require('./routes/scan');
+var heartbeatRouter = require('./routes/heartbeat');
+var updateRouter = require('./routes/update');
+var updatesizeRouter = require('./routes/updatesize')
 
 var app = express();
 
@@ -15,12 +19,18 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/scan', scanRouter);
+app.use('/heartbeat', heartbeatRouter);
+app.use('/update', updateRouter);
+app.use('/updatesize', updatesizeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
