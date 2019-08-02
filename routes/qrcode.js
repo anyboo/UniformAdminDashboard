5c9278a81d6ac04e190149324e6b830aa75dcf43
Kevin <anyboo@qq.com>
2019-08-02 16:00:08 +0800

    add QRCode

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var QRCode = require('qrcode')
+
+var opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/jpeg',
+  rendererOpts: {
+    quality: 0.3
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  QRCode.toFile('./public/images/qrcode.png', 'Some text', {
+    color: {
+      dark: '#00F', // Blue dots
+      light: '#0000' // Transparent background
+    }
+  }, function(err) {
+    if (err) throw err
+    console.log('done')
+  });
+
   res.render('qrcode', {
-    title: 'qrcode router'
+    title: 'qrcode router',
+    src: './public/images/qrcode.png'
   });
 });
 
